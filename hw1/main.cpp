9bf3b8ed7af2e89a363d8bfae6f0ae6a13d5a5fa
KNmuku <2915800366@qq.com>
2024-02-21 12:11:04 +0800

    finished hw0&hw1

--- hw1/main.cpp
+++ hw1/main.cpp
@@ -2,8 +2,10 @@
 #include "rasterizer.hpp"
 #include <eigen3/Eigen/Eigen>
 #include <iostream>
+#include <cmath>
 #include <opencv2/opencv.hpp>
 
+using namespace std;
 constexpr double MY_PI = 3.1415926;
 
 Eigen::Matrix4f get_view_matrix(Eigen::Vector3f eye_pos)
@@ -26,7 +28,13 @@ Eigen::Matrix4f get_model_matrix(float rotation_angle)
     // TODO: Implement this function
     // Create the model matrix for rotating the triangle around the Z axis.
     // Then return it.
-
+    float radian_angle = rotation_angle / 180.f * MY_PI;
+    Eigen::Matrix4f rotate;
+    rotate << cos(radian_angle), -sin(radian_angle), 0, 0,
+              sin(radian_angle), cos(radian_angle) , 0, 0,
+              0, 0, 1, 0,
+              0, 0, 0, 1;
+    model = rotate * model;
     return model;
 }
 
@@ -40,8 +48,53 @@ Eigen::Matrix4f get_projection_matrix(float eye_fov, float aspect_ratio,
     // TODO: Implement this function
     // Create the projection matrix for the given parameters.
     // Then return it.
+    float radian_fov = eye_fov / 180.f * MY_PI;
+    float top   = abs(zNear) * tan(radian_fov / 2);
+    float bot   = -top;
+    float right = top * aspect_ratio;
+    float left  = -right; 
 
+    Eigen::Matrix4f persp_to_ortho;
+    Eigen::Matrix4f translate;
+    Eigen::Matrix4f scale;
+    Eigen::Matrix4f ortho;
+    persp_to_ortho << zNear, 0, 0, 0,
+                      0, zNear, 0, 0,
+                      0, 0, (zNear + zFar), (-zNear) * zFar,
+                      0, 0, 1, 0;
+    translate   <<    1, 0, 0, -(left + right) / 2.f,
+                      0, 1, 0, -(bot + top) / 2.f,
+                      0, 0, 1, -(zNear + zFar) / 2.f,
+                      0, 0, 0, 1;
+    scale     <<      2.f/(right-left), 0, 0, 0,
+                      0, 2.f/(top-bot), 0, 0,
+                      0, 0, 2.f/(zNear-zFar), 0,
+                      0, 0, 0, 1;
+
+    ortho = scale * translate;
+    projection = ortho * persp_to_ortho * projection;
     return projection;
+}
+
+Eigen::Matrix4f get_rotation(Vector3f axis, float angle) {
+  float radian_angle = angle / 180.f * MY_PI;
+  Eigen::Matrix3f random_rotate;
+  Eigen::Matrix4f rotate = Eigen::Matrix4f::Identity();
+  Eigen::Matrix3f i = Eigen::Matrix3f::Identity();
+  Eigen::Matrix3f proj_to_axis, proj_to_compl, vert_to_plane;
+  // Corresponding to projection_to_axis,
+  //                  projection_to_complement,
+  //                  vertical_to_plane;
+  proj_to_axis       = axis * (axis.transpose());
+  proj_to_compl = (i - proj_to_axis) * cos(radian_angle);   
+  vert_to_plane   <<     0, -axis.z(), axis.y(),
+                             axis.z(), 0, -axis.x(),
+                             -axis.y(), axis.x(), 0;
+  vert_to_plane *= sin(radian_angle);
+  random_rotate = proj_to_axis + proj_to_compl + vert_to_plane;
+
+  rotate.block<3,3>(0,0) = random_rotate;
+  return rotate;
 }
 
 int main(int argc, const char** argv)
@@ -93,9 +146,14 @@ int main(int argc, const char** argv)
     while (key != 27) {
         r.clear(rst::Buffers::Color | rst::Buffers::Depth);
 
+        
+        //float ang = 45.f/180.f * MY_PI;
+        //Vector3f n;
+        //n << 0, -sin(ang), cos(ang);
+        //r.set_model(get_rotation(n, angle));
         r.set_model(get_model_matrix(angle));
         r.set_view(get_view_matrix(eye_pos));
-        r.set_projection(get_projection_matrix(45, 1, 0.1, 50));
+        r.set_projection(get_projection_matrix(45, 1, -0.1, -50)); // change zNear & zFar to be negative
 
         r.draw(pos_id, ind_id, rst::Primitive::Triangle);
 
